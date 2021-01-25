178821c3fb4333cd94a6480b815ac1e57bb5afd8
jmy <jmyme02@163.com>
2021-01-25 14:14:46 +0800

    提交主干升级内容

--- src/main/java/com/example/demo/git/Hello_Git.java
+++ src/main/java/com/example/demo/git/Hello_Git.java
@@ -4,5 +4,9 @@ public class Hello_Git {
     public static void main(String[] args) {
         //主干say hello
         System.out.println("hello git");
+        //主干丰富
+        System.out.println("doxxxxxBefore");
+
+        System.out.println("doxxxxxAfter");
     }
 }
